4f6d4b3cc6239f36614a1470227054094b023dc9
vaishnavi <gavhanevaishnavi135@mail.com>
2023-05-21 10:29:25 +0530

    initial commit

---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+
+  loginStatus:any=localStorage.getItem("loginStatus");
+  userRole:any=localStorage.getItem("userRole");
+  userName=localStorage.getItem("userName");
+
+  constructor(private router:Router)
+  {
+    if(this.loginStatus!="active")
+    {
+      this.router.navigate(['signin']);
+    }
+  }
+
+  logout()
+  {
+    localStorage.clear();
+    this.router.navigate(['signin']);
+    
+  }
+}
---
+++ src/app/component/signin/signin.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PostapiService } from 'src/app/service/postapi.service';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent {
+
+  loginStatus:any=localStorage.getItem("loginStatus");
+
+  constructor(private service:PostapiService, private router:Router)
+  {
+    if(this.loginStatus=="active")
+    {
+      this.router.navigate(['dashboard']);
+    }
+  }
+
+
+
+  signin(email:any, password:any)
+  {
+   
+    this.service.signin(email,password).subscribe(
+
+      response => {
+
+        console.log(response);
+        alert('login successful');
+        localStorage.setItem("loginStatus","active");
+        localStorage.setItem("email",email);
+        localStorage.setItem("userId",response.userId);
+        localStorage.setItem("userId",response.userId);
+        localStorage.setItem("userName",response.userName);
+        this.router.navigate(['dashboard']);
+    },
+
+    () => { alert('Wrong email Id or password!!');  }
+
+    );
+
+
+
+  }
+}
